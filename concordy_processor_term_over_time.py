3d39f40e6a5d873a83184728c5a34c2ee31c382c
Adam Mazel <43005538+amaze2@users.noreply.github.com>
2021-01-12 10:47:53 -0500

    Update concordy_processor_term_over_time.py

--- concordy_processor_term_over_time.py
+++ concordy_processor_term_over_time.py
@@ -39,12 +39,12 @@ years = range(start_year,end_year+1)
 lem_ans = st.sidebar.selectbox(label="Lemmatize the data?", options=("Yes","No"), index=0)
 if lem_ans == "Yes":
     #pickleIn = open("concordy_lemma.pickle",'rb')
-    pickleIn = requests.get("/content/drive/My Drive/concordy_app/concordy_raw_no_POS.pickle")
+    pickleIn = requests.get("http:///content/drive/My Drive/concordy_app/concordy_raw_no_POS.pickle")
     d_year_month_text = pickle.load(pickleIn)
     search_terms = [lemmatizer.lemmatize(term.lower()) for term in search_terms if term.isalpha()]
 else:
     #pickleIn = open("concordy_raw_no_POS.pickle",'rb')
-    pickleIn = requests.get("/content/drive/My Drive/concordy_app/concordy_raw_no_POS.pickle")
+    pickleIn = requests.get("http:///content/drive/My Drive/concordy_app/concordy_raw_no_POS.pickle")
     d_year_month_text = pickle.load(pickleIn)
     case_ans = st.sidebar.selectbox(label="Lower case the data?", options=("Yes","No"), index=0)
     if case_ans == "Yes":
